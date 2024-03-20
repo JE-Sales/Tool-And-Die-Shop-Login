c83014d7a86c6da4547a98c8a68736cd6c171aac
John Emmanuel Sales <66854142+JE-Sales@users.noreply.github.com>
2024-03-20 22:48:06 +0800

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     var userType = document.getElementById("userType").value;
     if (userType === "user" || userType === "admin") {
-        window.location.href = "add.html"; // Replace with your add page URL
+        window.location.href = "https://je-sales.github.io/Case-Project-1-Tool-and-Die-Shop/"; // Replace with your add page URL
         event.preventDefault(); // Prevent default form submission
     }
 });
