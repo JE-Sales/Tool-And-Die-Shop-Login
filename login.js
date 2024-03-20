ca1e9cefbf6140555e1939b5a118c54845c2a129
John Emmanuel Sales <66854142+JE-Sales@users.noreply.github.com>
2024-03-20 23:13:07 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,3 @@
-const createJobButton = document.getElementById("createJobButton");
-
-viewJobButton.addEventListener('click', (event) => {
-    viewJob();
-    viewJobForm.focus();
-});
-
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    var userType = document.getElementById("userType").value;
-    if (userType === "user" || userType === "admin") {
-        window.location.href = "https://je-sales.github.io/Case-Project-1-Tool-and-Die-Shop/"; // Replace with your add page URL
-        event.preventDefault(); // Prevent default form submission
-    }
-});
+function login() {
+  window.location.href = "https://je-sales.github.io/Case-Project-1-Tool-and-Die-Shop/";
+}
